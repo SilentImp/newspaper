8197d94364a2c457540344ed5b0d8014736093d6
Anton Nemtsev <newsilentimp@gmail.com>
2024-03-24 19:07:12 +0100

    feat(newspaper): :sparkles: adds header component

--- app/NewsPage.tsx
+++ app/NewsPage.tsx
@@ -5,14 +5,15 @@ import "./typography.css";
 
 import type NewsEvent from '@/types/NewsEvent';
 import { DataProvider } from "@/context/DataContext";
+import PageHeader from "./PageHeader";
 
 type NewsPage = {
   items: NewsEvent[];
 }
 
- const NewsPage = ({ items }: NewsPage) => (
+const NewsPage = ({ items }: NewsPage) => (
   <DataProvider value={items}>
-    <h1>Midnight Post</h1>
+    <PageHeader />
   </DataProvider>
 );
 
---
+++ app/PageHeader/PageHeader.tsx
@@ -0,0 +1,8 @@
+import './PageHeader.css'
+const PageHeader = () => (
+  <header className="page-header">
+     <h1 className="page-header__title">Midnight Post</h1>
+  </header>
+);
+
+export default PageHeader;
