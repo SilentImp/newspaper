7226846c2a1635e55017fc6dfa04bd42db72544d
Anton Nemtsev <newsilentimp@gmail.com>
2024-03-24 20:00:07 +0100

    feat(newspaper): :sparkles: adds TimeLineEvent component

---
+++ app/TimeLine/TimeLineEvent/TimeLineEvent.tsx
@@ -0,0 +1,39 @@
+import type NewsEvent from '@/types/NewsEvent';
+import './TimeLineEvent.css';
+
+type TimeLineEvent = {
+  event: NewsEvent;
+}
+
+const TimeLineEvent = (
+  {
+    event: {
+      timestamp,
+      title,
+    }
+  }: TimeLineEvent
+) => {
+  const date = new Date(timestamp);
+  let optionsTime: Intl.DateTimeFormatOptions = {
+    hour: "2-digit",
+    minute: "2-digit",
+  };
+  const time = Intl.DateTimeFormat('nl-NL', optionsTime).format(date);
+  const dateTime = Intl.DateTimeFormat('nl-NL', {
+    dateStyle: "full",
+    timeStyle: "full",
+  }).format(date);
+  return (
+    <li className="timeline-event">
+      <a href="#" className="timeline-event__link" >
+        <time 
+          className="timeline-event__time" 
+          title={dateTime} 
+          dateTime={timestamp}>{time}</time>
+        <span className="timeline-event__title">{title}</span>
+      </a>
+    </li>
+  );
+}
+
+export default TimeLineEvent;
