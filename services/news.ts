635de1457cbbc7e171b5c3eea8812027b6a6d510
Anton Nemtsev <newsilentimp@gmail.com>
2024-03-24 22:41:16 +0100

    feat(newspaper): :construction_worker: adds image build and publication

--- services/news.ts
+++ services/news.ts
@@ -2,7 +2,7 @@ import CONFIG from "@/config/client";
 
 class News {
   static get = async () => {
-    const response = await fetch(CONFIG.NewsURL);
+    const response = await fetch(CONFIG.NewsURL, { cache: 'no-store' });
     if (!response.ok) {
       throw new Error('Failed to fetch news list');
     }
