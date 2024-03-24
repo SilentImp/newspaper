5fc1ef31ac520e93696408584a8f1c851b805c8f
Anton Nemtsev <newsilentimp@gmail.com>
2024-03-24 20:02:53 +0100

    feat(newspaper): :sparkles: adds TimeLine component to the landing page

--- app/NewsPage.tsx
+++ app/NewsPage.tsx
@@ -7,6 +7,7 @@ import type NewsEvent from '@/types/NewsEvent';
 import { DataProvider } from "@/context/DataContext";
 import PageHeader from "./PageHeader";
 import TopNews from "./TopNews";
+import TimeLine from "./TimeLine";
 
 type NewsPage = {
   items: NewsEvent[];
@@ -16,6 +17,7 @@ const NewsPage = ({ items }: NewsPage) => (
   <DataProvider value={items}>
     <PageHeader />
     <TopNews />
+    <TimeLine />
   </DataProvider>
 );
 
